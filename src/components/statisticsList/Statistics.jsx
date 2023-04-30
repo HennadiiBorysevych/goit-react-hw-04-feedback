139de72775bd649ffba699b38ed8faf738071ce8
Hennadii Borysevych <genuch3@gmail.com>
2023-04-30 15:44:39 +0300

    refactoring

--- src/components/statisticsList/Statistics.jsx
+++ src/components/statisticsList/Statistics.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import { StatisticsList, StatisticsItem } from './Statistics.styled';
 import PropTypes from 'prop-types';
 
-class Statistics extends React.Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <StatisticsList>
-        <StatisticsItem>Good: {good}</StatisticsItem>
-        <StatisticsItem>Neutral: {neutral}</StatisticsItem>
-        <StatisticsItem>Bad: {bad}</StatisticsItem>
-        <StatisticsItem>Total: {total()}</StatisticsItem>
-        <StatisticsItem>
-          Positive feedback: {positivePercentage()}%
-        </StatisticsItem>
-      </StatisticsList>
-    );
-  }
-}
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <StatisticsList>
+      <StatisticsItem>Good: {good}</StatisticsItem>
+      <StatisticsItem>Neutral: {neutral}</StatisticsItem>
+      <StatisticsItem>Bad: {bad}</StatisticsItem>
+      <StatisticsItem>Total: {total()}</StatisticsItem>
+      <StatisticsItem>
+        Positive feedback: {positivePercentage()}%
+      </StatisticsItem>
+    </StatisticsList>
+  );
+};
+
 export default Statistics;
 
 Statistics.propTypes = {
